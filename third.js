f1a27ad631ccda017046830d0afab47d0cf635a2
shahid <kaushalkendra23@gmail>
2026-02-11 12:29:06 +0530

    my forth commit

---
+++ third.js
@@ -0,0 +1,18 @@
+let todos = [];
+
+// TODO: Add a task
+function addTask(task) {
+  // your code here
+}
+
+// TODO: Remove a task
+function removeTask(task) {
+  // your code here
+}
+
+addTask("Study JS");
+addTask("Workout");
+removeTask("Study JS");
+
+console.log(todos);
+
